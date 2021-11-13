aa1309fa8d7cbda1af367d669f9d8c74e6e3d7c1
r0xtar <72034576+r0xtar@users.noreply.github.com>
2021-11-14 00:53:11 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/iamLiquidX/MirLeechX")
-    buttons.buildbutton("Group", "https://t.me/mirrorLeechTelegramBot")
+    buttons.buildbutton("Repo", "https://github.com/login")
+    buttons.buildbutton("Group", "https://t.me/r0x74r")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -142,9 +142,9 @@ help_string_telegraph = f'''<br>
 <b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
 help = Telegraph(access_token=telegraph_token).create_page(
-        title='MirLeechX Help',
-        author_name='MirLeechX',
-        author_url='https://github.com/iamLiquidX/MirLeechX',
+        title='Bot Commands Guide',
+        author_name='r0x74r',
+        author_url='https://t.me/r0x74r',
         html_content=help_string_telegraph,
     )["path"]
 
